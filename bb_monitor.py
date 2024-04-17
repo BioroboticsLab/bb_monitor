d62e8fb91985bcc0e2759ebecbd41bf287e5eb55
jacobdavidson <jddavid@umich.edu>
2024-04-17 08:59:11 +0200

    Update bb_monitor.py

--- bb_monitor.py
+++ bb_monitor.py
@@ -73,13 +73,14 @@ def resize_image(image, width):
     resized_image = cv2.resize(image, (width, new_height), interpolation=cv2.INTER_AREA)
     return resized_image
 
-def add_text_to_image(image, text, position=(10, 15), font_scale_relative=0.01, font_thickness=1):
+def add_text_to_image(image, text, position=(0.02,0.02), font_scale_relative=0.0007, font_thickness=1):
     """Adds text to an image."""
     # Calculate font scale based on image width
-    (_, width) = image.shape[:2]
+    (height, width) = image.shape[:2]
     font_scale = font_scale_relative * width
+    position_scaled = [position[0]*width, position[1]*height]
 
-    cv2.putText(image, text, position, cv2.FONT_HERSHEY_SIMPLEX, font_scale, (255, 255, 255), font_thickness, cv2.LINE_AA)
+    cv2.putText(image, text, position_scaled, cv2.FONT_HERSHEY_SIMPLEX, font_scale, (255, 255, 255), font_thickness, cv2.LINE_AA)
     return image
 
 def send_message(message):
@@ -163,7 +164,7 @@ def wait_and_get_images():
                 if composite_image is not None:
                     composite_image = rotate_image(composite_image,config.rotate)
                     composite_image = resize_image(composite_image,width=config.image_width)
-                    composite_image = add_text_to_image(composite_image,config.monitor_bot_name,position=(config.image_width-200,15),font_scale_relative=0.015)
+                    composite_image = add_text_to_image(composite_image,config.monitor_bot_name,position=(0.5,0.02),font_scale_relative=0.0012)
                     # send image to message bot
                     process_image_and_send(composite_image)
                     print('Sent image at',datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
